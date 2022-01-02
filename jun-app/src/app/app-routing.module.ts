a10e7de938e65132536dd7382c061a247d9c90af
Just0a0Fred <85016719+Just0a0Fred@users.noreply.github.com>
2022-01-02 14:49:18 +0500

    1 done
    Сделан роутинг на странице, все работает отлично!

--- jun-app/src/app/app-routing.module.ts
+++ jun-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
+import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyYandexMapComponent } from './company-yandex-map/company-yandex-map.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LayoutComponent},
+  {path: 'list', component: CompanyListComponent},
+  {path: 'detail', component: CompanyDetailComponent},
+  {path: 'map', component: CompanyYandexMapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jun-app/src/app/app.module.ts
+++ jun-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
+import { CompanyYandexMapComponent } from './company-yandex-map/company-yandex-map.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyListComponent,
+    CompanyDetailComponent,
+    CompanyYandexMapComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
